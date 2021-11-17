4c1ec258a03353f95a97e9c18b5c7fa4fa4966b6
JanJJaniszewski <pole19901990@gmail.com>
2021-11-17 15:12:00 +0100

    added new file analyses with analyses

--- kernel/analyses.R
+++ kernel/analyses.R
@@ -14,9 +14,9 @@ f_hat_target_Gaussian_KRR = Predict_kernel_Ridge_MM( Gaussian_KRR_model_train,
 
 # Laplacian kernel
 Laplacian_KRR_model_train = Kernel_Ridge_MM( Y_train=y,kernel = 'Laplacian',
-                                            Matrix_covariates_train=X, method="RKHS", rate_decay_kernel=5.0)
+                                             Matrix_covariates_train=X, method="RKHS", rate_decay_kernel=5.0)
 f_hat_target_Laplacian_KRR = Predict_kernel_Ridge_MM( Gaussian_KRR_model_train, 
-                                                     Matrix_covariates_target=X )
+                                                      Matrix_covariates_target=X )
 
 # Compare prediction based on linear (i.e. RR-BLUP) and Gaussian kernel
 par(mfrow=c(1,3))
@@ -40,61 +40,9 @@ for (sigma in 5 * 10 ^ c(-2, -1, 1, 2)){
   Gaussian_KRR_model_train = Kernel_Ridge_MM( Y_train=y,kernel = 'Gaussian', init_sigma2K = sigma,
                                               Matrix_covariates_train=X, method="RKHS", rate_decay_kernel=5.0)
   prediction = Predict_kernel_Ridge_MM( Gaussian_KRR_model_train, 
-                                                       Matrix_covariates_target=X )
+                                        Matrix_covariates_target=X )
   plot(prediction, y)
   gridsearch_analysis[toString(sigma)] <- mean((prediction - y) ^ 2)
 }
 
 gridsearch_analysis
-
-# source('./Week_2/Caio_Elastic_net/functions.R')
-# 
-# library(tidyverse)
-# library(optimbase)
-# library(matlib)
-# library(fastDummies)
-# library(rdetools)
-# 
-# # Kernel function
-# kernel_rbf <- function (XtX, gamma) {
-#   XX <- matrix(1, n) %*% diag(XtX)
-#   k <- exp(-(XX - 2 * XtX + t(XX)) * gamma)
-#   return(k)
-# }
-# 
-# # Load and define data
-# load('./Week_3/Airline.RData')
-# input <- Airline
-# y <- input$output
-# X_names <- fastDummies::dummy_cols(input, select_columns = c('airline')) %>% names
-# input <- fastDummies::dummy_cols(input %>% scale, select_columns = c('airline'))
-# names(input) <- X_names
-# X <- input %>% select(-c(output, airline, year, airline_1)) %>% as.matrix 
-# n <- nrow(X)
-# lambda <- 0.5
-# gamma <- 1
-# ones <- matrix(1,n,1)
-# w0 <- mean(y)
-# 
-# # Transformation
-# XXt <- tcrossprod(X)
-# t(X) %*% X
-# X %*% t(X)
-# 
-# all(XtX == X %*% t(X))
-# 
-# X_rbf <- kernel_rbf(XtX, gamma)
-# X_rbf_X_rbf_t <- X_rbf%*%t(X_rbf)
-# q_tilde <- (I + lambda * (X_rbf_X_rbf_t)^(-1))^(-1) %*% J %*% y
-# y
-# 
-# I <- diag(n)
-# J <- I - n^(-1) * matrix(1, n, n)
-# ones_vector <- matrix(1, 1, n)
-# X_tilde <- J %*% X
-# 
-# 
-# 
-# L_ridge <- function(w0, q_tilde){
-#   
-# }
